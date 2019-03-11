1d9546af32da4448d3471fd711a2be445bba63c4
sergioebp <sergioebp@gmail.com>
2019-03-11 11:49:15 -0300

    implementado métodos rest para tiendas

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,4 +1,6 @@
 class StoresController < ApplicationController
+  before_action :set_store, only: [:show, :edit, :update, :destroy]
+
   def index
     @stores = Store.where('active = true')
     if user_signed_in?
@@ -11,7 +13,7 @@ class StoresController < ApplicationController
   end
 
   def show
-    @store = Store.find(params[:id])
+    @my_store = @store.users.where("user_id = #{current_user.id}").present?
   end
 
   def new 
@@ -22,13 +24,54 @@ class StoresController < ApplicationController
     @store = Store.new(
       name: params[:store][:name],
       description: params[:store][:description],
-      address: params[:store][:address]
+      address: params[:store][:address],
+      latitude: params[:store][:latitude],
+      longitude: params[:store][:longitude]
     )
     @store.save
-    coordinates = Geocoder.coordinates(@store.address)
-    @store.latitude = coordinates.first
-    @store.longitude = coordinates.last
-    @store.save 
+    if @store.latitude.nil? || @store.longitude.nil?
+      coordinates = Geocoder.coordinates(@store.address)
+      @store.latitude = coordinates.first
+      @store.longitude = coordinates.last
+      @store.save
+    end
+    @store.users << current_user
+    @store.save
+    current_user.role = 1
+    current_user.save
     redirect_to store_path(@store)
   end
+
+  def edit
+  end
+
+  def update
+    @store.name = params[:store][:name]
+    @store.description = params[:store][:description]
+    @store.address = params[:store][:address]
+    @store.latitude = params[:store][:latitude]
+    @store.longitude = params[:store][:longitude]
+    @store.save
+    if @store.latitude.nil? || @store.longitude.nil?
+      coordinates = Geocoder.coordinates(@store.address)
+      @store.latitude = coordinates.first
+      @store.longitude = coordinates.last
+      @store.save
+    end
+    redirect_to store_path(@store)
+  end
+
+  def destroy
+    @store.destroy
+    if !(current_user.stores.present?)
+      current_user.role = 0
+      current_user.save
+    end
+    redirect_to stores_path
+  end
+
+  private
+    def set_store
+      @store = Store.find(params[:id])
+    end
 end
